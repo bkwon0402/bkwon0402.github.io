7d5e3a1c50604a10d9f094c462ec97ceac36c945
Chi Hung <minhchau.nguyen14@gmail.com>
2022-08-07 17:51:51 +0700

    remove no neeed

--- js/main.js
+++ js/main.js
@@ -28,17 +28,3 @@ $('.d-flex-media').each(function(index, value) {
         $(this).find(".title-sp").html(all.join());
     }
 });
-
-
-function passwordCheck(){
-    var password = prompt("Please enter the password.");
-    if (password==="ilikepie"){
-        window.location="realpage.html";
-    } else if (password!='' && password!=null) {
-        while(password !=="ilikepie"){
-            password = prompt("Please enter the password.");
-        }
-        window.location="realpage.html";
-    }
-}
-window.onload=passwordCheck;
